70601c569bb9b9615b7af7981b50b1d7103602c2
Anthony Malary <amalary@bu.edu>
2022-03-08 14:14:07 -0800

    Finish 3 and 4

--- Classes-Lab.js
+++ Classes-Lab.js
@@ -1,3 +1,5 @@
+
+// # Number 1
 class Person {
     constructor(name,age,eyes,hair){
         this.legs = 2;
@@ -13,7 +15,7 @@ class Person {
     introduction(){
         console.log(`Greetings may name is ${this.name} and I'm about ${this.age} years old `)
     }
-}
+};
 
 const me = new Person('Anthony',26, 'brown eyes', 'black'); 
 
@@ -21,6 +23,8 @@ console.log(me);
 
 me.introduction()
 
+
+// # Number 2
 class Pet {
 
     constructor(name,breed,size,color,sound){
@@ -42,10 +46,70 @@ class Pet {
         console.log("begins to pee on the rug")
     }
 
-}
+};
 
 const dog = new Pet('Wilfred','Greyhound','Big','Grey','bark bark'); 
 
 console.log(dog); 
 
 dog.growl()
+
+//  # Number 3 
+
+class Boat {
+    constructor(name,color,size,amountPassengers,direction){
+
+        this.name = name; 
+        this.color = color; 
+        this.size = size;
+        this.amountPassengers =amountPassengers; 
+        this.direction = direction; 
+    }
+    location(){
+        console.log('Somewhere in the Carribean')
+    }
+    dropAnchor(){
+        console.log('down goes the Anchor on you guys')
+    }
+    Sail(){
+        console.log('begin to stir the sail for somewhere else')
+    }
+};
+
+const boat = new Boat('Betsy','Pearl','Medium',33.,'North West');
+
+console.log(boat); 
+
+boat.dropAnchor()
+
+// # Number 4 
+
+class Artist {
+
+    constructor(name,style,hometown,age,inspiration){
+
+        this.name = name;
+        this.style = style;
+        this.hometown = hometown; 
+        this.age = age; 
+        this.inspiration = inspiration;
+    }
+    introduction(nameOfPerson){
+        console.log(`Hello ${nameOfPerson} my name is ${this.name} and I have been painting since I was a child`)
+    }
+    favorite(){
+        console.log('My favorite artist is Jean-Michel Basquiat if I can be honest with you')
+    }
+    dress(){
+        console.log("Well today I'm wearing Gorgio Armani ")
+    }
+}; 
+
+const artist = new Artist('Jean-Jaque','Contemporary','Brooklyn, New York', 21, 'Andy Warhol'); 
+
+console.log(artist); 
+
+artist.introduction('Anthony'); 
+
+
+
